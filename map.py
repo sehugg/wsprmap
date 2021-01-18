ab2445a62cd692f94e3113041cb23c1a6b598e05
Steven Hugg <hugg@fasterlight.com>
2021-01-18 16:38:24 -0600

    greyscale, fixed render error (?)

--- map.py
+++ map.py
@@ -1,10 +1,16 @@
-from gcmap import GCMapper
-import gzip, csv
+from gcmap import GCMapper, Gradient
+import gzip, csv, sys, random
 from datetime import datetime
 import maidenhead as mh
 import numpy as np
 
 filename = 'wsprspots-2020-12.csv.gz'
+
+grad = Gradient([(   0,   32,   32,   32),
+                 (   1,  255,  255,  255)])
+
+def r():
+    return random.random()*0.5
 
 class RouteMap:
     def __init__(self):
@@ -19,44 +25,50 @@ class RouteMap:
             self.routes.add(route)
             rxloc = mh.to_location(rxgrid)
             txloc = mh.to_location(txgrid)
-            self.lats1.append(txloc[0])
-            self.lons1.append(txloc[1])
-            self.lats2.append(rxloc[0])
-            self.lons2.append(rxloc[1])
+            if len(txloc) == 2 and len(rxloc) == 2:
+                self.lats1.append(txloc[0]+r())
+                self.lons1.append(txloc[1]+r())
+                self.lats2.append(rxloc[0]+r())
+                self.lons2.append(rxloc[1]+r())
             #print(txgrid,rxgrid,txloc,rxloc)
     def draw(self, filename):
-        gcm = GCMapper()
-        gcm.set_data(self.lons1, self.lats1, self.lons2, self.lats2)
-        img = gcm.draw()
-        img.save(filename)
+        if len(self.lons1) == len(self.lats1) == len(self.lons2) == len(self.lats2) and len(self.lons1) > 5:
+            gcm = GCMapper(width=640, cols=grad, line_width=1)
+            gcm.set_data(self.lons1, self.lats1, self.lons2, self.lats2)
+            img = gcm.draw()
+            img.save(filename)
 
 
-def drawmap(freq):
+def drawmaps():
     maps = {}
     with gzip.open(filename, mode='rt') as f:
         reader = csv.reader(f)
         for row in reader:
             band = int(row[12])
-            if band == freq:
-                time = datetime.utcfromtimestamp(int(row[1]))
-                rxgrid = row[3][0:4]
-                txgrid = row[7][0:4]
-                dbm = int(row[8])
-                key = (time.month,time.hour,band,dbm)
-                map = maps.get(key)
-                if not map:
-                    map = RouteMap()
-                    maps[key] = map
-                    print(key)
-                    #snr = int(row[4])
-                    #dist = int(row[10])
-                map.addroute(txgrid,rxgrid)
+            time = datetime.utcfromtimestamp(int(row[1]))
+            rxgrid = row[3][0:4]
+            txgrid = row[7][0:4]
+            dbm = int(row[8])
+            #snr = int(row[4])
+            #dist = int(row[10])
+            key = (time.year,time.month,time.hour,band,dbm)
+            map = maps.get(key)
+            if not map:
+                map = RouteMap()
+                maps[key] = map
+                print(key)
+            map.addroute(txgrid,rxgrid)
+            #if key[2] == 12:
+            #    break
     return maps
 
-allmaps = drawmap(10)
+allmaps = drawmaps()
 for k,m in allmaps.items():
     print (k,m)
-    fn = 'output/map-%d-%d-%d-%d.png' % k
-    m.draw(fn)
-#img = gcm.draw()
-#img.save('output.png')
+    pngfn = 'output/map-%d-%d-%d-%d-%d.png' % k
+    try:
+        m.draw(pngfn)
+    except ValueError: # TODO???
+        print(pngfn, sys.exc_info())
+    except IndexError: # TODO???
+        print(pngfn, sys.exc_info())
